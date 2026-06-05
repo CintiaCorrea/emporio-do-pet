665775473558c6b9036bedc7f99a530775ac45b7
CintiaCorrea <gestao.emporiodopet@gmail.com>
2026-06-05 18:06:33 -0300

    feat: registrar BcPollCronService no IntegrationsModule

--- backend/src/modules/integrations/integrations.module.ts
+++ backend/src/modules/integrations/integrations.module.ts
@@ -1,5 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BotConversaController } from './botconversa.controller';
+import { BcPollCronService } from './bc-poll-cron.service';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [BotConversaController],
+  providers: [BcPollCronService],
+})
+export class IntegrationsModule {}
+import { Module } from '@nestjs/common';
+import { BotConversaController } from './botconversa.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
